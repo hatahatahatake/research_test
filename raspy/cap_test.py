dcbf6a086b4733346694b0bc8cf2d6ae8c4771ee
畠山友華 <yuka@MacBook-Pro-2.local>
2018-07-04 02:51:38 +0900

    一定時間における画像取得完了

--- raspy/cap_test.py
+++ raspy/cap_test.py
@@ -1,29 +1,27 @@
-# Webカメラ 画像取得コードのテスト
-# OpenCV のインポート
+# Webカメラ 画像取得コード　6/27~
+
 import cv2
 import glob
 import os
 import sys
+import time
 from os import getpid
 from os import kill
 from threading import Timer
 from time import sleep
-from signal import SIGTERM
 
 # VideoCaptureのインスタンスを作成する。
 # 引数でカメラを選べれる。
 # 0は内蔵カメラ、1は入力カメラ
 cap = cv2.VideoCapture(1)
 cap.set(4, 700)  # Width
-cap.set(4, 500)  # Heigh
+cap.set(4, 600)  # Heigh
 cap.set(5, 15)   # FPS
 
 sys.path.append('Desktop/iMac-Desktop/git/research_test/raspy/images')
 
 # ディレクトリパス指定
 setnumber = "cap_img"
-dir_path = u"./images"
-#output_path = "C:Desktop/iMac-Desktop/git/research_test/raspy/images"
 output_path = "./images"
 output_name = output_path + '/' + setnumber
 
@@ -31,24 +29,28 @@ output_name = output_path + '/' + setnumber
 import os
 print(os.path.exists(output_path))
 
+print('start')
+
 i = 1;
 while True:
     # VideoCaptureから1フレーム読み込む
     ret, frame = cap.read()
 
-    # 画像を取得
+    # sleep を使う方法
+    time.sleep(30)
+
+    # 動画を取得
     cv2.imshow('Raw Frame', frame)
+
+    # raspy/image ディレクトリに取得画像を自動保存
+    cv2.imwrite(output_name + str(i) + ".png", frame)
+    i += 1
 
     # キー入力を1ms待って、k が27（ESC）だったらBreakする
     k = cv2.waitKey(1)
-    if k == ord('c'):
-
-        # raspy/image ディレクトリに取得画像を保存
-        cv2.imwrite(output_name + str(i) + ".png", frame)
-        i += 1
-
-    elif k == 27:
+    if k == 27:
         break
+        print('end')
 
 # キャプチャをリリースして、ウィンドウをすべて閉じる
 cap.release()
